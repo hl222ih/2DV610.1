cc10b849ea6423f55fbb5672d0f35e8d62035bc5
Hannes <hl222ih@student.lnu.se>
2015-11-09 05:13:59 +0100

    RED

--- 2DV610.Test/UnitTest.cs
+++ 2DV610.Test/UnitTest.cs
@@ -21,7 +21,7 @@ namespace _2DV610.Test
         [Fact]
         public void CorrectPathTypeTest()
         {
-            Line line = new Line("");
+            Line line = new Line("L 0 0 64 64");
             Circle circle = new Circle("");
             HalfCircle halfCircle = new HalfCircle("");
             QuarterCircle quarterCircle = new QuarterCircle("");
@@ -34,6 +34,16 @@ namespace _2DV610.Test
             Assert.True(quarterCircle.GetPathType().Equals(PathType.QuarterCircle));
             Assert.True(eighthCircle.GetPathType().Equals(PathType.EighthCircle));
             Assert.True(heart.GetPathType().Equals(PathType.Heart));
+        }
+
+        [Fact]
+        public void InvalidLineArgumentsThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new Line(""));
+            Assert.Throws<ArgumentException>(() => new Line("R 0 0 0 0"));
+            Assert.Throws<ArgumentException>(() => new Line("L 0 0 0 0"));
+            Assert.Throws<ArgumentException>(() => new Line("L 0 0 64 64 64"));
+            Assert.Throws<ArgumentException>(() => new Line("L 0 64 64"));
         }
     }
 }
