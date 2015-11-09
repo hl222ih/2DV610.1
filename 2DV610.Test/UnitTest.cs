5bec53131270352bffbbd3b9db15b79ce8530913
Hannes <hl222ih@student.lnu.se>
2015-11-09 02:00:32 +0100

    Initial commit

---
+++ 2DV610.Test/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+using Xunit;
+//using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace _2DV610.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
