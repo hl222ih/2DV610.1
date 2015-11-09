072a1140e3dbb30bf277d061b98d4710ac1c8aa2
Hannes <hl222ih@student.lnu.se>
2015-11-09 05:49:12 +0100

    GREEN

--- 2DV610/Classes/Line.cs
+++ 2DV610/Classes/Line.cs
@@ -8,8 +8,41 @@ namespace _2DV610.Classes
 {
     public class Line : Path
     {
+        int startX;
+        int startY;
+        int endX;
+        int endY;
+
+        /// <summary>
+        /// Creates a Line path.
+        /// </summary>
+        /// <param name="path">"L startX startY diffX diffY"</param>
         public Line(String path) : base(PathType.Line, path)
         {
+            string[] pathArr = path.Split(' ');
+            if (pathArr.Length != 5) throw new ArgumentException("Not correct number of arguments in string.");
+            if (!pathArr[0].Equals("L")) throw new ArgumentException("Not correct path type given in first string argument.");
+
+            int diffX;
+            int diffY;
+
+            try
+            {
+                string s = pathArr[0];
+                startX = int.Parse(pathArr[1]);
+                startY = int.Parse(pathArr[2]);
+                diffX = int.Parse(pathArr[3]);
+                diffY = int.Parse(pathArr[4]);
+            }
+            catch
+            {
+                throw new ArgumentException("X and Y values must be integers");
+            }
+
+            if (diffX == 0 && diffY == 0)
+            {
+                throw new ArgumentException("Line may not be of zero length");
+            }
         }
     }
 }
