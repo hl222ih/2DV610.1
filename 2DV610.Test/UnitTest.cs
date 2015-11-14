179db683dabf7f871fdfaaafcb91de788579b01c
Hannes <hl222ih@student.lnu.se>
2015-11-14 04:57:16 +0100

    RED

--- 2DV610.Test/UnitTest.cs
+++ 2DV610.Test/UnitTest.cs
@@ -419,13 +419,23 @@ namespace _2DV610.Test
         }
 
         [Fact]
-        public void PathCommandTest()
+        public void MPathCommandTest()
         {
-            //string[] elements = new string[] { "M", "18", "64" };
-            string svgPath = "M18,24";
-            PathCommand command = new PathCommand(svgPath);
-            Assert.True(command.GetRelativePath() == "m18,24");
-            Assert.True(command.GetAbsolutePath() == "M18,24");
+            string svgPath1 = "M18,24";
+            string svgPath2 = "m18,24";
+            PathCommand command1 = new PathCommand(svgPath1);
+            PathCommand command2 = new PathCommand(svgPath1, 10, 20);
+            PathCommand command3 = new PathCommand(svgPath2);
+            PathCommand command4 = new PathCommand(svgPath2, 10, 20);
+            Assert.True(command1.GetRelativePath() == "m18,24");
+            Assert.True(command1.GetAbsolutePath() == "M18,24");
+            Assert.True(command2.GetRelativePath() == "m8,4");
+            Assert.True(command2.GetAbsolutePath() == "M18,24");
+            Assert.True(command3.GetRelativePath() == "m18,24");
+            Assert.True(command3.GetAbsolutePath() == "M18,24");
+            Assert.True(command4.GetRelativePath() == "m18,24");
+            Assert.True(command4.GetAbsolutePath() == "M28,44");
+
         }
 
     }
