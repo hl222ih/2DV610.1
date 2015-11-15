36245ba16b037aa20ebbb73f9e3818c06711cd43
Hannes <hl222ih@student.lnu.se>
2015-11-15 04:54:32 +0100

    GREEN

--- 2DV610/Classes/PathCommand.cs
+++ 2DV610/Classes/PathCommand.cs
@@ -9,6 +9,24 @@ namespace _2DV610.Classes
 {
     public class PathCommand
     {
+        private enum CType
+        {
+            Undefined,
+            MoveTo, //M
+            ClosePath, //Z
+            LineTo, //L
+            HorizontalLineTo, //H
+            VerticalLineTo, //V
+            CurveTo, //C (cubic)
+            ShorthandCurveTo, //S (cubic)
+            QuadraticCurveTo, //Q
+            ShorthandQuadraticCurveTo, //T
+            EllipticalArc //A
+
+        }
+
+        private CType type;
+
         private string[] relativeElements;
         private string[] absoluteElements;
 
@@ -17,10 +35,78 @@ namespace _2DV610.Classes
         public float EndX { get; private set; }
         public float EndY { get; private set; }
 
-        public float CenterX { get; private set; }
-        public float CenterY { get; private set; }
-        public float RadiusX { get; private set; }
-        public float RadiusY { get; private set; }
+        public float CenterX
+        {
+            get
+            {
+                float cx = 0;
+
+                switch (type)
+                {
+                    case CType.MoveTo:
+                        cx = EndX;
+                        break;
+                    default:
+                    break;
+                }
+
+                return cx;
+            }
+        }
+        public float CenterY
+        {
+            get
+            {
+                float cy = 0;
+
+                switch (type)
+                {
+                    case CType.MoveTo:
+                        cy = EndY;
+                        break;
+                    default:
+                        break;
+                }
+
+                return cy;
+            }
+        }
+        public float RadiusX
+        {
+            get
+            {
+                float rx = 0;
+
+                switch (type)
+                {
+                    case CType.EllipticalArc:
+                        rx = -1; //add logic
+                        break;
+                    default:
+                        break;
+                }
+
+                return rx;
+            }
+        }
+        public float RadiusY
+        {
+            get
+            {
+                float ry = 0;
+
+                switch (type)
+                {
+                    case CType.EllipticalArc:
+                        ry = -1; //add logic
+                        break;
+                    default:
+                        break;
+                }
+
+                return ry;
+            }
+        }
 
         public PathCommand(string svgCommandPath, float startX = 0, float startY = 0)
         {
@@ -50,30 +136,35 @@ namespace _2DV610.Classes
             switch (elems[0])
             {
                 case "M":
+                    type = CType.MoveTo;
                     EndX = float.Parse(elems[1]);
                     EndY = float.Parse(elems[2]);
                     relativeElements[1] = (EndX - StartX).ToString();
                     relativeElements[2] = (EndY - StartY).ToString();
                     break;
                 case "m":
+                    type = CType.MoveTo;
                     EndX = float.Parse(elems[1]) + StartX;
                     EndY = float.Parse(elems[2]) + StartY;
                     absoluteElements[1] = EndX.ToString();
                     absoluteElements[2] = EndY.ToString();
                     break;
                 case "A":
+                    type = CType.EllipticalArc;
                     EndX = float.Parse(elems[6]);
                     EndY = float.Parse(elems[7]);
                     relativeElements[6] = (EndX - StartX).ToString();
                     relativeElements[7] = (EndY - StartY).ToString();
                     break;
                 case "a":
+                    type = CType.EllipticalArc;
                     EndX = float.Parse(elems[6]) + StartX;
                     EndY = float.Parse(elems[7]) + StartY;
                     absoluteElements[6] = EndX.ToString();
                     absoluteElements[7] = EndY.ToString();
                     break;
                 default:
+                    type = CType.Undefined;
                     break;
             }
 
@@ -133,32 +224,69 @@ namespace _2DV610.Classes
 
         public bool IsMoveToCommand()
         {
-            throw new NotImplementedException();
+            return (type == CType.MoveTo);
         }
 
         public bool IsArcCommand()
         {
-            throw new NotImplementedException();
+            return (type == CType.EllipticalArc);
         }
 
         public bool IsUpper()
         {
-            throw new NotImplementedException();
+            bool isUpper = false;
+            CType[] undefined = new CType[] { CType.Undefined, CType.EllipticalArc, CType.CurveTo, CType.ShorthandCurveTo, CType.QuadraticCurveTo, CType.QuadraticCurveTo, CType.ShorthandQuadraticCurveTo };
+
+            if (undefined.Contains(type))
+            {
+                //adjust isUpper
+                throw new NotImplementedException("Not implemented for the command type");
+            }
+
+            return isUpper;
         }
 
         public bool IsLower()
         {
-            throw new NotImplementedException();
+            bool isLower = false;
+            CType[] undefined = new CType[] { CType.Undefined, CType.EllipticalArc, CType.CurveTo, CType.ShorthandCurveTo, CType.QuadraticCurveTo, CType.QuadraticCurveTo, CType.ShorthandQuadraticCurveTo };
+
+            if (undefined.Contains(type))
+            {
+                //adjust isLower
+                throw new NotImplementedException("Not implemented for the command type");
+            }
+
+            return isLower;
         }
 
         public bool IsRight()
         {
-            throw new NotImplementedException();
+            bool isRight = false;
+            CType[] undefined = new CType[] { CType.Undefined, CType.EllipticalArc, CType.CurveTo, CType.ShorthandCurveTo, CType.QuadraticCurveTo, CType.QuadraticCurveTo, CType.ShorthandQuadraticCurveTo };
+
+            if (undefined.Contains(type))
+            {
+                //adjust isRight
+                throw new NotImplementedException("Not implemented for the command type");
+            }
+
+            return isRight;
         }
 
         public bool IsLeft()
         {
-            throw new NotImplementedException();
+            bool isLeft = false;
+            CType[] undefined = new CType[] { CType.Undefined, CType.EllipticalArc, CType.CurveTo, CType.ShorthandCurveTo, CType.QuadraticCurveTo, CType.QuadraticCurveTo, CType.ShorthandQuadraticCurveTo };
+
+            if (undefined.Contains(type))
+            {
+                //adjust isLeft
+                throw new NotImplementedException("Not implemented for the command type");
+            }
+
+            return isLeft;
         }
+
     }
 }
