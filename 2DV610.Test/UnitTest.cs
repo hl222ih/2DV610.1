4dd4e30494504a63215d9fe118ee8447ad2581b8
Hannes <hl222ih@student.lnu.se>
2015-11-15 12:10:08 +0100

    RED

--- 2DV610.Test/UnitTest.cs
+++ 2DV610.Test/UnitTest.cs
@@ -425,6 +425,21 @@ namespace _2DV610.Test
         }
 
         [Fact]
+        public void SymbolOfRightHalfCircleTest()
+        {
+            Shape shape = new RightHalfCircle(50, 96, 32);
+
+            Symbol symbol1 = new Symbol("M82,64 A 32,32 0 0,1 82,128"); //absolute upper to lower
+            Symbol symbol2 = new Symbol("M82,64 a 32,32 0 0,1 0,64"); //relative upper to lower
+            Symbol symbol3 = new Symbol("M82,128 A 32,32 0 1,0 82,64"); //absolute lower to upper
+            Symbol symbol4 = new Symbol("M82,128 a 32,32 0 1,0 0,-64"); //relative lower to upper
+            Assert.True(symbol1.Contains(shape));
+            Assert.True(symbol2.Contains(shape));
+            Assert.True(symbol3.Contains(shape));
+            Assert.True(symbol4.Contains(shape));
+        }
+
+        [Fact]
         public void SymbolTest()
         {
             Symbol symbol = new Symbol("M18,64 A32,32 0 0,1 82,64");
