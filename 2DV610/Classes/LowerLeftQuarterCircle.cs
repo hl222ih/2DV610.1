0ef5818c7bc56c4cf61f2c508c0e9333636b8996
Hannes <hl222ih@student.lnu.se>
2015-11-22 10:48:46 +0100

    GREEN

--- 2DV610/Classes/LowerLeftQuarterCircle.cs
+++ 2DV610/Classes/LowerLeftQuarterCircle.cs
@@ -8,9 +8,12 @@ namespace _2DV610.Classes
 {
     public class LowerLeftQuarterCircle : QuarterCircle
     {
-        public LowerLeftQuarterCircle(int cx, int cy, int radius) : base(cx, cy, radius, ShapeType.Unspecified)
+        public LowerLeftQuarterCircle(int cx, int cy, int radius) : base(cx, cy, radius, ShapeType.LowerLeftQuarterCircle)
         {
-
+            X = cx - radius;
+            Y = cy;
+            Height = radius;
+            Width = radius;
         }
     }
 }
