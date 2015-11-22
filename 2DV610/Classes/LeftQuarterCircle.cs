9d05c2ed65d8614983cd358e7d118be98fc144e2
Hannes <hl222ih@student.lnu.se>
2015-11-22 12:39:27 +0100

    GREEN

--- 2DV610/Classes/LeftQuarterCircle.cs
+++ 2DV610/Classes/LeftQuarterCircle.cs
@@ -8,9 +8,17 @@ namespace _2DV610.Classes
 {
     public class LeftQuarterCircle : QuarterCircle
     {
-        public LeftQuarterCircle(int cx, int cy, float radius) : base(cx, cy, (int)radius, ShapeType.Unspecified)
-        {
+        public new float X { get; protected set; }
+        public new float Width { get; protected set; }
 
+        public LeftQuarterCircle(int cx, int cy, float radius) : base(cx, cy, (int)Math.Round(radius / Math.Sqrt(2) * 1.5, 0), ShapeType.LeftQuarterCircle)
+        {
+            Height = (int)Math.Round(radius / Math.Sqrt(2) * 2, 0);
+            base.Width = Height / 4;
+            Width = radius - Height / 2;
+            Y = cy - Height / 2;
+            base.X = cx - base.Width;
+            X = cx - radius;
         }
     }
 }
