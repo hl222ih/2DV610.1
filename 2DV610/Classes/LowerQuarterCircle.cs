bf40540c399abf95059eb5c184077317a08c9fc8
Hannes <hl222ih@student.lnu.se>
2015-11-22 12:27:05 +0100

    GREEN

--- 2DV610/Classes/LowerQuarterCircle.cs
+++ 2DV610/Classes/LowerQuarterCircle.cs
@@ -8,8 +8,15 @@ namespace _2DV610.Classes
 {
     public class LowerQuarterCircle : QuarterCircle
     {
-        public LowerQuarterCircle(int cx, int cy, float radius) : base(cx, cy, (int)radius, ShapeType.Unspecified)
+        public new float Height { get; protected set; }
+
+        public LowerQuarterCircle(int cx, int cy, float radius) : base(cx, cy, (int)Math.Round(radius / Math.Sqrt(2) * 1.5, 0), ShapeType.LowerQuarterCircle)
         {
+            Width = (int)Math.Round(radius / Math.Sqrt(2), 0) * 2;
+            base.Height = Width / 4;
+            Height = radius - Width / 2;
+            X = cx - Width / 2;
+            Y = cy;
 
         }
     }
