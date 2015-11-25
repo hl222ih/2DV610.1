b50cde1f282dc51af3f0aeb9776191eb23b12250
Hannes <hl222ih@student.lnu.se>
2015-11-25 04:30:57 +0100

    RED

---
+++ 2DV610.Test/SymbolServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+using _2DV610;
+using _2DV610.Classes;
+using System.Collections.Generic;
+
+namespace _2DV610.Test
+{
+    public class SymbolServiceTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public SymbolServiceTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        private class SymbolRepositoryMock : ISymbolRepository
+        {
+            public List<Symbol> GetAllSymbols()
+            {
+                return new List<Symbol>();
+            }
+        }
+
+        [Fact]
+        public void ConstructorTest()
+        {
+            ISymbolRepository mock = new SymbolRepositoryMock();
+            
+            SymbolService sut = new SymbolService(mock);
+        }
+    }
+}
---
+++ 2DV610/Classes/SymbolService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DV610.Classes
+{
+    public class SymbolService
+    {
+        public SymbolService(ISymbolRepository repo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
